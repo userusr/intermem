71d39d29e4284323b0933e978e0e6458e632869c
userusr <userusr@yandex.ru>
2020-03-25 17:30:18 +0300

    Simple connection

--- intermem/client.py
+++ intermem/client.py
@@ -1,8 +1,36 @@
 """Dummy memcached client library."""
+import socket
 
 
 class Client():
     """A simple client to memcached database for interview perposes."""
-    def __init__(self):
-        """Create only."""
-        pass
+    def __init__(self, host: str = '127.0.0.1', port: int = 11211) -> None:
+        """Constructor.
+
+        :param host: IP address or host name, defaults to '127.0.0.1'
+        :type host: str, optional
+        :param port: TCP port, defaults to 11211
+        :type port: int, optional
+        """
+        self.host = host
+        self.port = port
+        self.sock = None
+        self._connect()
+
+    def _connect(self):
+        self.close()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            sock.connect((self.host, self.port))
+        except Exception:
+            sock.close()
+            raise
+
+        self.sock = sock
+
+    def close(self):
+        """Close socket."""
+        if self.sock is not None:
+            self.sock.close()
+            self.sock = None
