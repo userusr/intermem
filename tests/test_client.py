fa6ae11d6fbc81ae53e5970e759d9011a5ac97ca
userusr <userusr@yandex.ru>
2020-03-25 17:29:23 +0300

    Add test for connection with real server

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 """Testing dummy memcached module."""
+import os
 import unittest
 
 from intermem.client import Client
 
 
 class TestIntermem(unittest.TestCase):
-    """Client class."""
-    def test_client(self):
-        """Dummy test."""
-        assert isinstance(Client(), Client)
+    """Test client."""
+    @unittest.skipIf(os.getenv('MEMCACHED_HOST') is None,
+                     'Define "MEMCACHED_HOST" to test real connection')
+    def test_real_connection(self):
+        """Test connection to real memcached server."""
+        host = os.getenv('MEMCACHED_HOST')
+        client = Client(host)
+        self.assertEqual(client.sock.getpeername()[0], host)
+        self.assertEqual(client.sock.getpeername()[1], 11211)
+        client.close()
