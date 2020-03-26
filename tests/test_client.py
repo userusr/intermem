e9adcb74c84b7f808d83e83781177224e3a8d746
userusr <userusr@yandex.ru>
2020-03-26 16:09:38 +0300

    Add tests on mock socket

--- tests/test_client.py
+++ tests/test_client.py
@@ -30,6 +30,10 @@ class MockSocket():
         """Mock."""
         pass
 
+    def sendall(self, _: bytes) -> None:
+        """Mock."""
+        pass
+
     def getpeername(self) -> Tuple[str, int]:
         """Mock of getpeername function.
 
@@ -76,7 +80,7 @@ class TestIntermem(unittest.TestCase):
         cmd = client._cmd_recv(b'name', b'key')
         self.assertEqual(cmd, b'name key\r\n')
 
-    def test_cmd_delete(self) -> None:
+    def test_cmd_maint(self) -> None:
         """Test generation of delete command."""
         client = Client()
         cmd = client._cmd_maint(b'name', b'key')
@@ -84,22 +88,47 @@ class TestIntermem(unittest.TestCase):
 
     def test_readlines_from_socket(self) -> None:
         """Test readline from socket."""
-        client = Client()
-        sock = MockSocket(client, b'STORED\r\n')
-        client.connect = mock.Mock(
-            side_effect=functools.partial(setattr, client, 'sock', sock))
-        client.connect()
-
+        client = self._get_mock_client(b'STORED\r\n')
         lines = client._readlines()
         self.assertListEqual(lines, [b'STORED'])
 
+    def test_cmd_set(self) -> None:
+        """Test set command."""
+        client = self._get_mock_client(b'STORED\r\n')
+        stored = client.cmd_set(b'key', b'value')
+        self.assertTrue(stored)
+
+        client = self._get_mock_client(b'ERROR\r\n')
+        stored = client.cmd_set(b'key', b'value')
+        self.assertFalse(stored)
+
+    def test_cmd_get(self) -> None:
+        """Test get command."""
+        client = self._get_mock_client(b'VALUE key 0 5\r\nvalue\r\nEND\r\n')
+        self.assertEqual(client.cmd_get(b'key1'), b'value')
+
+        client = self._get_mock_client(b'END\r\n')
+        self.assertIsNone(client.cmd_get(b'key_unknown'))
+
+    def test_cmd_delete(self) -> None:
+        """Test delete command."""
+        client = self._get_mock_client(b'DELETED\r\n')
+        self.assertTrue(client.cmd_delete(b'key1'))
+
+        client = self._get_mock_client(b'ERROR\r\n')
+        self.assertFalse(client.cmd_delete(b'key_unknown'))
+
     def test_mock_socket(self) -> None:
         """Connect to mock socket."""
-        client = Client()
-        sock = MockSocket(client)
-        client.connect = mock.Mock(
-            side_effect=functools.partial(setattr, client, 'sock', sock))
-        client.connect()
+        client = self._get_mock_client()
         self.assertEqual(client.sock.getpeername()[0], client.host)
         self.assertEqual(client.sock.getpeername()[1], client.port)
         client.close()
+
+    def _get_mock_client(self, recv_buf: Optional[bytes] = None) -> Client:
+        client = Client()
+        sock = MockSocket(client, recv_buf)
+        client.connect = mock.Mock(
+            side_effect=functools.partial(setattr, client, 'sock', sock))
+        client.connect()
+        return client
